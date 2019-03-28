2b618f70f485c8b0ece46dc7d6900ea09181e35c
Stuart P. Bentley <stuart@testtrack4.com>
2019-03-28 10:20:24 +0000

    Final unstaged changes before I overhaul this whole project

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 /* global cre */
 
+var closedTri = '\u25B6';
+var openTri = '\u25BC';
+
 var teBox = cre('.box',{wall: true},[
-  cre('.box-title',{part: 'title', contentEditable: true},'Box'),
-  cre('.box-children',{part: 'child-boxes'}),
-  cre('button.new-child',{type: 'button', part:'new-child-button'},'New box')
+  cre('.box-headline',[
+    cre('.box-title',{part: 'title', contentEditable: true},'Box'),
+    cre('button.box-toggle',{type: 'button', part:'open-toggle'})
+  ]),
+  cre('.box-contents', {part: 'contents'}, [
+    cre('.box-children',{part: 'child-boxes'}),
+    cre('button.new-child',{type: 'button', part:'new-child-button'},'New box')
+  ]),
 ]);
 
 function createChildBox() {
   var elBox = cre(teBox);
+
   var doc = {title: 'Box'};
   var childBox = {
     doc: doc,
     element: elBox
   };
+
+  var boxOpen = false;
+
+  var elContents = elBox.getPart('contents');
+  var elToggle = elBox.getPart('open-toggle');
+  elToggle.textContent = closedTri;
+  elContents.hidden = true;
+
+  elToggle.addEventListener('click', function(evt) {
+    boxOpen = !boxOpen;
+    if (boxOpen) {
+      elContents.hidden = false;
+      elToggle.textContent = openTri;
+    } else {
+      elContents.hidden = true;
+      elToggle.textContent = closedTri;
+    }
+  });
 
   var elTitle = elBox.getPart('title');
   elTitle.addEventListener('input', function(evt) {
